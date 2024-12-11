5ebdad979fb046b27eb15252055392eb88a309ce
Ruiyang Wang <wry0416@outlook.com>
2024-12-11 12:42:39 +0000

    dynamic movement

--- src/movement.js
+++ src/movement.js
@@ -1,11 +1,27 @@
 import { Noise } from 'noisejs';
+import { numBodies } from './ws.js';
 
 const noise = new Noise(Math.random());
 
+const action = {
+    numBodies: 0,
+    energy: 0, // 0-100
+    energyLimit: 100,
+    energyDecay: 0.05,
+    energyGain: 0.1,
+    energyNorm: 0,
+    update(numBodies) {
+        this.numBodies = numBodies;
+        this.energy = Math.max(0, this.energy - this.energyDecay);
+        this.energy = Math.min(this.energyLimit, this.energy + this.energyGain * numBodies);
+        this.energyNorm = this.energy / this.energyLimit;
+
+    }
+};
 const movement = {
     pause: false,
-    movMul: 0,
-    noiseMul: 0.5,
+    zero: false,
+    noiseMul: 0.1,
 
     controlSeg1: [0, 0],
     controlSeg2: [0, 0],
@@ -15,25 +31,40 @@ const movement = {
 
     time: Date.now(),
 
-    update(delta) {
-        this.controlSeg1[0] = noise.simplex2(this.time, 0) * this.rangeSeg1;
-        this.controlSeg1[1] = noise.simplex2(0, this.time) * this.rangeSeg1;
+    update(deltaTime) {
+        if (this.zero) {
+            this.controlSeg1 = [0, 0];
+            this.controlSeg2 = [0, 0];
+            return;
+        }
 
-        this.controlSeg2[0] = noise.simplex2(this.time, 100) * this.rangeSeg2;
-        this.controlSeg2[1] = noise.simplex2(100, this.time) * this.rangeSeg2;
+        action.update(numBodies);
+        const noiseH = this.pNoise(100, 2);
+        const noiseL = this.pNoise(200, 1);
+        const noiseCombined = noiseH.map((val, i) => val * this.noiseMul + noiseL[i] * (1 - this.noiseMul));
+
+        this.controlSeg1[0] = noiseCombined[0] * this.rangeSeg1 * action.energyNorm;
+        this.controlSeg1[1] = noiseCombined[1] * this.rangeSeg1 * action.energyNorm;
+
+        this.controlSeg2[0] = noiseCombined[2] * this.rangeSeg2 * action.energyNorm;
+        this.controlSeg2[1] = noiseCombined[3] * this.rangeSeg2 * action.energyNorm;
+
         if (this.pause) {
             return;
         }
-        if (typeof delta === 'number' && !isNaN(delta)) {
-            this.time += 0.0002 * delta;
+        if (typeof deltaTime === 'number' && !isNaN(deltaTime)) {
+            this.time += 0.00015 * deltaTime;
         }
     },
 
-    pNoise(offset) {
-        return [noise.simplex2(this.time, offset), noise.simplex2(offset, this.time)];
+    pNoise(offset, mult) {
+        return [noise.simplex2(this.time * mult, 0),
+        noise.simplex2(0, this.time * mult),
+        noise.simplex2(this.time * mult, offset),
+        noise.simplex2(offset, this.time * mult)];
     },
 
-    searchTarget(target){
+    searchTarget(target) {
 
     }
 };
@@ -41,6 +72,9 @@ const movement = {
 addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
         movement.pause = !movement.pause;
+    }
+    if (event.key === 'z') {
+        movement.zero = !movement.zero;
     }
 });
 
